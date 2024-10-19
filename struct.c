de5aab42f7890718497fe8e91a48f50e8bbe3acb
Gabriel Borges <161774213+devgabrielsborges@users.noreply.github.com>
2024-10-19 15:13:37 -0300

    Update struct.c

--- struct.c
+++ struct.c
@@ -3,16 +3,3 @@
 //
 
 #include "struct.h"
-
-
-// extern typedef struct {
-//     char login[10];  //strcmp("borges", "borges") -> 1
-//     char senha[20];
-//     char nome[20];
-//     char endereco[50];
-//     char contato[9];
-//     char nascimento[10];  // dd/mm/aaaa
-// } dado_usuario;
-//
-//
-// extern dado_usuario dados[10];
